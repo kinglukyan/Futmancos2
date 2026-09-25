7f4da12f9b0d8917888d55a800c885dc8c52ffe0
kinglukyan <santoslucasalmeida@gmail.com>
2026-09-25 11:55:25 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'futmancos-shell-v12';
-const APP_SHELL = ['/', '/offline.html', '/manifest.webmanifest', '/assets/app-icon-192.png', '/assets/app-icon-512.png', '/assets/apple-touch-icon.png', '/assets/logo-futmancos.jpeg', '/assets/card-texture.png'];
+const CACHE_NAME = 'futmancos-shell-v13';
+const APP_SHELL = ['/', '/offline.html', '/manifest.webmanifest', '/assets/app-icon-192.png', '/assets/app-icon-512.png', '/assets/apple-touch-icon.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
